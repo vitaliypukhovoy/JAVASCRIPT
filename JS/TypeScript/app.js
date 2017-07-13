d03f07851b3b337c080530f95d4be4c263982b1d
vitah <vitaliypukhovoy@gmail.com>
2017-07-13 21:09:26 +0300

    1

---
+++ JS/TypeScript/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by ������������� on 13.07.2017.
+ */
+console.log('lkjlkjkljlk');
+//# sourceMappingURL=app.js.map
---
+++ JS/TypeScript/scrpts.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ������������� on 13.07.2017.
+ */
+//# sourceMappingURL=scrpts.js.map
