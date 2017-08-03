54e5e80d204a09fb89db0a37836ffc74a4b09986
vitah <vitaliypukhovoy@gmail.com>
2017-08-03 19:31:16 +0300

    typescript

--- JS/TypeScript/app.js
+++ JS/TypeScript/app.js
@@ -91,11 +91,80 @@ if (typeof finalValue == 'number') {
 // canAlsoBeNull = null;
 // let canThisBeAny :any = null;
 // canThisBeAny = 12;
+//ECMA 6
 //----------------------------------------------------------------------
 var myName1 = 'Max1';
 var age1 = 30;
 var anything;
 anything = 12;
 //arrow function
+console.log('ARROW FUNCTION');
 var addNumber = function (number1, number2) {
+    return number1 + number2;
 };
+console.log(addNumber(10, 3));
+var multyplyNumber = function (number1, number2) {
+    return number1 * number2;
+};
+console.log(multyplyNumber(3, 4));
+var multyplyNumber2 = function (number1, number2) { return number1 * number2; };
+console.log(multyplyNumber(3, 5));
+var greet = function () {
+    console.log('Hello');
+};
+greet();
+var greetFriend = function (friend) { return console.log(friend); };
+greetFriend("Manu");
+// Default parameters
+console.log('Default PARAMETERS');
+var countDown = function (start, end) {
+    if (start === void 0) { start = 10; }
+    if (end === void 0) { end = start - 5; }
+    console.log(20);
+    while (start > 0) {
+        start--;
+    }
+    ;
+    console.log("Done", start);
+    console.log("Done", end);
+};
+countDown(20);
+// rest & Spread operator
+console.log('REST & SPREAD');
+var numbers = [1, 10, 99, -5];
+console.log(Math.max(33, 99, 10, -3)); //list of numbers
+console.log(Math.max.apply(Math, numbers)); //array of numbers
+function makeArray(arg1, arg2) {
+    return [arg1, arg2];
+}
+;
+function makeArray2(name) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    return args;
+}
+;
+console.log(makeArray(1, 2));
+console.log(makeArray2("Max", 1, 2, 3));
+//Destructing array
+console.log('DISTRACTING');
+var myHobbies = ["Cooking", "Sport"];
+var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
+// const  hobby1 = myHobbies[0];
+// const hobby2 = myHobbies[1];
+console.log(hobby1, hobby2);
+//Destructing  object
+var userData1 = { userName: "Max", ages: 27 };
+//const  userName = userData1.userName;
+//const {userName, ages} = userData1;
+var myName3 = userData1.userName, myAge = userData1.ages;
+console.log(myName3, myAge);
+// Template literal
+console.log('Template literal');
+var userName = "Max";
+//const greeting = "Hello, I'm " + userName;
+var greeting = " This is a heading!\nI'm " + userName + "\nThis is cool";
+console.log(greeting);
+// 
--- JS/TypeScript/app.ts
+++ JS/TypeScript/app.ts
@@ -134,11 +134,10 @@ let userData :{name : string; age : number} =
 // let canThisBeAny :any = null;
 // canThisBeAny = 12;
 
-
+//ECMA 6
 //----------------------------------------------------------------------
 
 let myName1 :string = 'Max1';
-
 
 let age1 : number = 30;
 
@@ -146,12 +145,87 @@ let anything ;
 
 anything = 12;
 
-
-
-
-
 //arrow function
-const addNumber = function (number1: number, number2: number) {
+console.log('ARROW FUNCTION')
+const addNumber = function (number1: number, number2: number):number {
 
-
+return number1+ number2;
 }
+
+console.log(addNumber(10,3))
+
+const  multyplyNumber = (number1: number, number2: number) => {
+   return number1*number2;
+}
+console.log(multyplyNumber(3,4))
+const  multyplyNumber2 = (number1: number, number2: number) => number1*number2;
+console.log(multyplyNumber(3,5))
+
+const  greet = ()=>{
+    console.log('Hello')
+};
+greet();
+
+const  greetFriend = friend  => console.log(friend);
+
+greetFriend("Manu");
+
+// Default parameters
+console.log('Default PARAMETERS');
+const countDown = ( start:number = 10, end : number = start -5):void => {
+    console.log(20);
+  while (start >0) {
+      start--;
+  };
+  console.log("Done",start);
+  console.log("Done",end);
+};
+countDown(20);
+
+// rest & Spread operator
+console.log('REST & SPREAD');
+const   numbers  = [1,10,99,-5];
+console.log(Math.max(33,99,10,-3)); //list of numbers
+console.log(Math.max(...numbers)); //array of numbers
+
+function  makeArray(arg1: number, arg2: number){
+  return [arg1,arg2];
+};
+
+function  makeArray2(name:string,...args: number []){
+    return args;
+};
+console.log(makeArray(1,2));
+
+console.log(makeArray2("Max",1,2,3));
+
+//Destructing array
+
+console.log('DISTRACTING');
+const myHobbies = ["Cooking","Sport"];
+const [hobby1, hobby2] = myHobbies;
+// const  hobby1 = myHobbies[0];
+// const hobby2 = myHobbies[1];
+console.log(hobby1 , hobby2);
+
+//Destructing  object
+
+const  userData1 = {userName:"Max", ages:27};
+//const  userName = userData1.userName;
+//const {userName, ages} = userData1;
+const {userName: myName3, ages :myAge} = userData1;
+console.log(myName3, myAge);
+
+// Template literal
+
+console.log('Template literal');
+const userName = "Max";
+//const greeting = "Hello, I'm " + userName;
+const greeting = ` This is a heading!
+I'm ${userName}
+This is cool`;
+console.log(greeting);
+
+
+
+//
