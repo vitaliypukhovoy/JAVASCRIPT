9bc19b684d8dd441b1042489afe357759ab58772
vitah <vitaliypukhovoy@gmail.com>
2017-09-02 17:43:25 +0300

    typescript

--- JS/ES6/ES6/Arrow_this.js
+++ JS/ES6/ES6/Arrow_this.js
@@ -30,4 +30,19 @@
 })();
 
 // third version
-var a = {arr:[5], method : function(){  return ()=>console.log(this.arr)}};
+var a1 = {arr:[5], method : function(){  return ()=>console.log(this.arr)}};
+
+//fourth version
+
+var arr = [8];
+var a2 = {
+    arr: [7],
+    method1: function myMethod() {
+        console.log(this.arr);
+    },
+    method2: ()=> {
+        console.log(arr);}
+
+};
+a2.method1();
+a2.method2();
