edd04d11e74c2155c4c134a54d17a80c9a681601
vitah <vitah@gmail.com>
2018-03-26 13:12:58 +0300

    1

---
+++ JS/Angular5/SequenceTasks/SequenceTasks/src/app/serv/serv.task.ts
@@ -0,0 +1,20 @@
+export class Task {
+  public number :number;
+  public task; string;
+
+  constructor(number, task)
+  {
+    this.number = this.number;
+    this.task = task;
+  };
+
+  // get number():number
+  // {
+  //   return this.number;
+  // };
+  //
+  // get task(): string
+  // {
+  //   return this.string;
+  // }
+}
